1523ad2a055e589e29edb78b84cc54f385d2687d
Anthony2be <adubovitsky1234@gmail.com>
2025-05-24 19:18:47 -0700

    last update

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -7,15 +7,17 @@ extends Area2D
 @onready var spawn_timer: Timer = $SpawnTimer
 
 
+func _ready():
+	spawn_timer.start()
+
 func spawn() -> void:
 	randomize()
 	var upper = upper_left.global_position
 	var lower = bottom_right.global_position
-	var thing = milk.instance()
+	var thing = milk.instantiate()
 	thing.position.x = position.x + lerp(upper.x, lower.x, randf())
 	thing.position.y = position.y + lerp(lower.y, upper.y, randf())
-	get_parent().add_child(thing)
-
+	add_child(thing)
 
 
 func _on_spawn_timer_timeout() -> void:
