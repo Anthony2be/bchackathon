4a59b258297d5bee650767e423437ceba7b730a5
Anthony2be <adubovitsky1234@gmail.com>
2025-05-24 18:47:15 -0700

    updates

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -13,35 +13,30 @@ extends CharacterBody2D
 func _ready() -> void:
 	navigation_agent_2d.target_position = point1.global_position
 	sprite.play("default")
-	timer.start()
+	#timer.start()
+	navigation_agent_2d.target_desired_distance = 20
 
 func _physics_process(_delta: float):
 	point1.position.y = global_position.y
 	point2.position.y = global_position.y
 	if !is_on_floor():
 		velocity.y = min(velocity.y + grav, 1000)
-	#navigation_agent_2d.distance_to_target()
-	#navigation_agent_2d.target_position = point1.global_position
-	#if navigation_agent_2d.target_reached:
+	
+	if !navigation_agent_2d.is_navigation_finished():
 		
-		#print("reached")
-		#timer.start()
-	var dir = Vector2.ZERO
-	dir = navigation_agent_2d.get_next_path_position() - global_position
-	dir = dir.normalized().x
-	dir = 0 if is_equal_approx(dir, 0) else dir
+		if (navigation_agent_2d.target_position.x - global_position.x) < 0:
+			velocity.x = -speed
+		else:
+			velocity.x = speed
+	else:
+		print("reached")
+		velocity = Vector2.ZERO
+		position = navigation_agent_2d.target_position
 	
-	if dir < 0:
+	if velocity.x < 0:
 		sprite.flip_h = true
-	elif dir > 0: 
+	else:
 		sprite.flip_h = false
-	
-	if !navigation_agent_2d.is_target_reached():
-		velocity.x = dir * speed
-	
-	if velocity.x > -1 && velocity.x < 1:
-		sprite.play("default")
-	
 		
 	#if navigation_agent_2d.target_reached:
 		#print("reached")
@@ -50,16 +45,17 @@ func _physics_process(_delta: float):
 
 func _on_timer_timeout() -> void:
 	if navigation_agent_2d.target_reached:
-		print("runnin")
+		#print("runnin")
 		sprite.play("Running")
 		if navigation_agent_2d.target_position == point1.global_position:
 			navigation_agent_2d.target_position = point2.global_position
 		else:
 			navigation_agent_2d.target_position = point1.global_position
-	print('started')
+	#print('started')
 	#timer.stop()
 
 
 func _on_navigation_agent_2d_target_reached() -> void:
-	print("reached")
+	#print("reached")
+	sprite.play("default")
 	timer.start()
